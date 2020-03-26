2dff6b3c8cd557ca69c78928c724715204564eaa
caoweijian <caoweijian@kedacom.com>
2020-03-26 15:46:56 +0800

    集成轮播图控件

---
+++ banner/src/main/java/com/caowj/banner/BannerSwitcher.kt
@@ -0,0 +1,39 @@
+package com.caowj.banner
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ViewAnimator
+
+/**
+ * 轮播广告横幅组件的自动播放部分。
+ *
+ * 在可以在其中添加自定义的组件
+ */
+internal class BannerSwitcher : ViewAnimator {
+
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attributes: AttributeSet?) : super(context, attributes) {
+        Log.i("AutoBanner", "BannerSwitcher constructor method")
+        // set default animation if anim attributes not set
+        inAnimation = inAnimation ?: AnimationUtils.loadAnimation(context, R.anim.slide_fade_in)
+        outAnimation = outAnimation ?: AnimationUtils.loadAnimation(context, R.anim.slide_fade_out)
+        animateFirstView = false
+    }
+
+    /**
+     * 一次添加多个子view。会在原有的子view后边追加新的子View。
+     *
+     * @param children 可变参数，可以传入需要添加的View
+     */
+    fun addViews(children: Array<out View>) {
+        val count = childCount
+        children.forEachIndexed { index, view ->
+            addView(view, count + index, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT))
+        }
+    }
+
+}
---
+++ banner/src/main/java/com/caowj/banner/anim/SimpleAnimationListener.kt
@@ -0,0 +1,17 @@
+package com.caowj.banner.anim
+
+import android.view.animation.Animation
+
+/**
+ * 简化的AnimationListener根据需要进行方法的override。
+ */
+internal abstract class SimpleAnimationListener : Animation.AnimationListener {
+    override fun onAnimationRepeat(animation: Animation) {
+    }
+
+    override fun onAnimationEnd(animation: Animation) {
+    }
+
+    override fun onAnimationStart(animation: Animation) {
+    }
+}
